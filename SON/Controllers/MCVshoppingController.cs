46391a3db7e8258d666dbcd1e01b1deff9db192d
Amorntep <yim29na@gmail.com>
2017-05-02 17:19:20 +0700

    first commit

---
+++ SON/Controllers/MCVshoppingController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SON.Models;
+using SON.DAO;
+
+namespace SON.Controllers
+{
+    public class MCVshoppingController : Controller
+    {
+        //
+        // GET: /MCVshopping/
+        MCVshoppingDAO dbo = new MCVshoppingDAO();
+        DatabaseMVCshoppingEntities entity = new DatabaseMVCshoppingEntities();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View(dbo.Showcategories());
+        }
+
+        public ActionResult insertproduct()
+        {
+            ViewBag.list = Selectcategorise();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult insertproduct(products product)
+        {
+            dbo.InsertProduct(product);
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UpdateProductID"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult editporduct(int UpdateProductID)
+        {
+            ViewBag.list = Selectcategorise();
+            products product = dbo.Getproduct(UpdateProductID);
+            return View(product);
+
+        }
+
+        [HttpPost]
+        public ActionResult editporduct(products product)
+        {
+            dbo.Update(product);
+            return RedirectToAction("", "");
+        }
+
+        public List<SelectListItem> Selectcategorise()
+        {
+            List<SelectListItem> listCategory = new List<SelectListItem>();
+            List<categories> list = dbo.Getcategories();
+            foreach (categories cd in list)
+            {
+                SelectListItem item = new SelectListItem();
+                item.Text = cd.CategoryName;
+                item.Value = cd.CategoryID.ToString();
+                listCategory.Add(item);
+            }
+            return listCategory;
+        }
+     
+        public ActionResult Login()
+        {
+            Session["CustomerID"] = null;
+            Session["ContactName"] = null;
+            Session["Status"] = null;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(customers Customers)
+        {
+            
+            var chk = entity.customers.Where(x => x.ContactName == Customers.ContactName && x.Password == Customers.Password).FirstOrDefault();
+            if (chk != null)
+            {
+                Session["CustomerID"] = chk.CustomerID.ToString();
+                Session["ContactName"] = chk.ContactName.ToString();
+                Session["Status"] = chk.Status;
+                return RedirectToAction("Index");
+            }
+            return View(Customers);
+        }
+
+        [HttpGet]
+        public ActionResult Regiters()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Regiters(customers Member)
+        {
+            dbo.save(Member);
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Showproducts(int id)
+        {
+            return View(dbo.Showwheyproduct(id));
+        }
+        public ActionResult showmember()
+        {
+            return View(dbo.ShowMemberAll());
+        }
+        public ActionResult Showbays()
+        {
+            return View(dbo.ShowbaysAll());
+        }
+        [HttpGet]
+        public ActionResult CartMember(int id)
+        {
+            return View(dbo.ShowbaysMember(id));
+        }
+        [HttpGet]
+        public ActionResult CartProduct()
+        {
+            string date = Request["OrderDate"];
+            int id = Convert.ToInt32(Request["CustomerID"]);
+            string status = Request["status"];
+            int ProductID = Convert.ToInt32(Request["ProductID"]);
+            orders order = new orders();
+            order.OrderDate = date;
+            order.CustomerID = id;
+            order.status = status;
+            order.ProductID = ProductID;
+            dbo.AddProductMember(order);
+            return RedirectToAction("CartMember", new { id = Session["CustomerID"] });
+        }
+        [HttpGet]
+        public ActionResult DelCart(int id)
+        {
+            dbo.DeleteCart(id);
+            return RedirectToAction("CartMember", new { id = Session["CustomerID"] });
+        }
+        public ActionResult Pay()
+        {
+            int count = entity.orders.Count(x => x.status == "0");
+            for (int i = 0; i < count; i++)
+            {
+                dbo.Pay();
+            }
+            return RedirectToAction("History", new { id = Session["CustomerID"] });
+        }
+        [HttpGet]
+        public ActionResult History(int id)
+        {
+            return View(dbo.History(id));
+        }
+        [HttpGet]
+        public ActionResult SendMember(int id)
+        {
+            dbo.Send(id);
+            return RedirectToAction("Showbays");
+        }
+	}
+}
---
+++ SON/DAO/MCVshoppingDAO.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SON.Models;
+
+namespace SON.DAO
+{
+    public class MCVshoppingDAO
+    {
+        DatabaseMVCshoppingEntities entity = new DatabaseMVCshoppingEntities();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="customer"></param>
+        public void save(customers customer)
+        {
+            entity.customers.Add(customer);
+            entity.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buys"></param>
+        public void Insertbuys(orders buys)
+        {
+            entity.orders.Add(buys);
+            entity.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="insertproduct"></param>
+        public void InsertProduct(products insertproduct)
+        {
+            entity.products.Add(insertproduct);
+            entity.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="editProducts"></param>
+        public void Update(products editProducts)
+        {
+            var productDB = entity.products.Where(x => x.ProuctID == editProducts.ProuctID).FirstOrDefault();
+            productDB.CategoryID = editProducts.CategoryID;
+            productDB.ProductName = editProducts.ProductName;
+            productDB.UnitsInStock = editProducts.UnitsInStock;
+            productDB.Price = editProducts.Price;
+
+            entity.Entry(productDB).State = System.Data.EntityState.Modified;
+            entity.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="deleteProductID"></param>
+        public void Deleteproducts(int deleteProductID)
+        {
+            var del = entity.products.Where(x => x.ProuctID == deleteProductID).FirstOrDefault();
+            entity.Entry(del).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="DeleteCustomerID"></param>
+        public void Deletemember(int DeleteCustomerID)
+        {
+            var del = entity.customers.Where(x => x.CustomerID == DeleteCustomerID).FirstOrDefault();
+            entity.Entry(del).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="DeleteOrderID"></param>
+        public void DeleteOrder(int DeleteOrderID)
+        {
+            var dels = entity.orders.Where(x => x.OrderID == DeleteOrderID).FirstOrDefault();
+            entity.Entry(dels).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+        
+
+        public List<categories> Getcategories()
+        {
+            List<categories> list = new List<categories>();
+            list = entity.categories.ToList();
+            return list;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Productid"></param>
+        /// <returns></returns>
+        public products Getproduct(int Productid)
+        {
+            return entity.products.Where(x => x.ProuctID == Productid).FirstOrDefault();
+        }
+
+
+        public List<categories> Showcategories()
+        {
+            List<categories> list = new List<categories>();
+            list = entity.categories.ToList();
+            return list;
+        }
+
+
+        public List<products> Showwheyproduct(int productid)
+        {
+            List<products> list = new List<products>();
+            list = entity.products.Where(x => x.CategoryID == productid).ToList();
+            return list;
+        }
+        public List<customers> ShowMemberAll()
+        {
+            List<customers> list = new List<customers>();
+            list = entity.customers.ToList();
+            return list;
+        }
+        public List<orders> ShowbaysAll()
+        {
+            List<orders> list = new List<orders>();
+            list = entity.orders.Where(x => x.status != "0").ToList();
+            return list;
+        }
+        public List<orders> ShowbaysMember(int id)
+        {
+            List<orders> list = new List<orders>();
+            list = entity.orders.Where(x => x.status == "0" & x.CustomerID == id).ToList();
+            return list;
+        }
+        public void AddProductMember(orders order)
+        {
+            entity.orders.Add(order);
+            entity.SaveChanges();
+        }
+        public void DeleteCart(int id)
+        {
+            var dels = entity.orders.Where(x => x.OrderID == id).FirstOrDefault();
+            entity.Entry(dels).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+        public List<orders> History(int id)
+        {
+            List<orders> list = new List<orders>();
+            list = entity.orders.Where(x => x.status != "0" & x.CustomerID == id).ToList();
+            return list;
+        }
+        public void Pay()
+        {
+            orders order = new orders();
+            var productDB = entity.orders.Where(x => x.status == "0").FirstOrDefault();
+            productDB.status = "1";
+            entity.Entry(productDB).State = System.Data.EntityState.Modified;
+            entity.SaveChanges();
+        }
+        public void Send(int id)
+        {
+            orders order = new orders();
+            var productDB = entity.orders.Where(x => x.OrderID == id).FirstOrDefault();
+            productDB.status = "2";
+            entity.Entry(productDB).State = System.Data.EntityState.Modified;
+            entity.SaveChanges();
+        }
+    }
+}
